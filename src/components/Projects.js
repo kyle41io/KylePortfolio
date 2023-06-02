c629334e7ee50e9b355b35adec1913a14c5e9a08
kyle41io <vankien41hust@gmail.com>
2023-06-02 16:50:56 +0700

    Add modifies

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,6 +7,8 @@ import { GithubIcon } from './Icons'
 import project1 from '../../public/images/projects/EntranceTest_Pj.png'
 import project2 from '../../public/images/projects/First_blog.png'
 import project3 from '../../public/images/projects/Kyle_Audio.png'
+import project4 from '../../public/images/projects/MovieAPI.png'
+
 
 import { motion } from 'framer-motion'
 
@@ -17,25 +19,25 @@ const FeaturedProject = ({type, title, summary, img, link, github }) => {
 
   return(
     <div>
-      <article className='w-ful flex items-center justify-between rounded-3xl border border-solid border-dark dark:border-light bg-light dark:bg-dark shadow-2xl p-12 relative rounded-br-2xl'> 
+      <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark dark:border-light bg-light dark:bg-dark shadow-2xl p-12 relative rounded-br-2xl'> 
 
         <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark dark:bg-light rounded-br-3xl '/>
 
-        <Link href={link} target="_blank" className='ml-7 w-2/5 cursor-pointer overflow-hidden rounded-lg'>
+        <a href={link} target="_blank" className='ml-7 w-2/5 cursor-pointer overflow-hidden rounded-lg'>
           <MotionImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}}
           priority sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw, 50vw"/>
-        </Link>
+        </a>
 
         <div className='w-1/2 flex flex-col items-start justify-between pl-6'> 
           <span className='font-medium text-myblue dark:text-primaryDark dark:text-shadow-DarkShadow text-xl'>{type}</span>
-          <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+          <a href={link} target="_blank" className='hover:underline underline-offset-2'>
           <h2 className='my-2 w-full text-left text-4xl font-bold text-dark dark:text-light'>{title}</h2>
-          </Link>
+          </a>
 
           <p className='my-2 font-medium text-dark dark:text-light '>{summary}</p>
           <div className='mt-2 flex items-center'>
-            <Link href={github} target="_blank" className='w-10'><GithubIcon /> </Link>
-            <Link href={link} target="_blank" className='ml-12 rounded-lg bg-myblue dark:bg-primaryDark text-yellow-200 dark:text-dark/80 p-1 px-4 text-lg font-semibold'>Visit Project</Link>
+            <a href={github} target="_blank" className='w-10'><GithubIcon /> </a>
+            <a href={link} target="_blank" className='ml-12 rounded-lg bg-myblue dark:bg-primaryDark text-yellow-200 dark:text-dark/80 p-1 px-4 text-lg font-semibold'>Visit Project</a>
 
           </div>
         </div>
@@ -50,18 +52,18 @@ const Project =({type, title, img, link, github}) => {
 
         <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light rounded-br-3xl'/>
 
-        <Link href={link} target="_blank" className='ml-2 w-full cursor-pointer overflow-hidden rounded-lg'>
+        <a href={link} target="_blank" className='ml-2 w-full cursor-pointer overflow-hidden rounded-lg'>
           <MotionImage src={img} alt={title} className='w-full h-auto' whileHover={{scale:1.05}}/>
-        </Link>
+        </a>
 
         <div className='w-full flex flex-col items-start justify-between mt-4'> 
           <span className='font-medium text-myblue dark:text-primaryDark dark:text-shadow-DarkShadow text-xl'>{type}</span>
-          <Link href={link} target="_blank" className='hover:underline underline-offset-2'>
+          <a href={link} target="_blank" className='hover:underline underline-offset-2'>
           <h2 className='my-2 w-full text-left text-3xl font-bold text-dark dark:text-light'>{title}</h2>
-          </Link>
+          </a>
           <div className='w-full mt-2 flex items-center justify-between'>
-            <Link href={github} target="_blank" className='w-10 ml-4'><GithubIcon /> </Link>
-            <Link href={link} target="_blank" className='mx-4 rounded-lg bg-myblue dark:bg-primaryDark text-yellow-200 dark:text-dark/80 p-1 px-4 text-lg font-semibold'>Visit</Link>
+            <a href={github} target="_blank" className='w-10 ml-4'><GithubIcon /> </a>
+            <a href={link} target="_blank" className='mx-4 rounded-lg bg-myblue dark:bg-primaryDark text-yellow-200 dark:text-dark/80 p-1 px-4 text-lg font-semibold'>Visit</a>
 
           </div>
         </div>
@@ -77,7 +79,8 @@ const Projects = () => {
         <div className='grid grid-cols-12 gap-24 gap-y-32 '>
 
           <div className='col-span-12'>
-            <FeaturedProject title="Entrance Test App by Power Apps" summary="An application so that candidates can check their competencies when applying to the company. Done using Microsoft's low-code system - Power Apps." link="https://apps.powerapps.com/play/e/default-41ab42a9-381b-48cf-8a85-720464922976/a/24f9b052-e11b-4c73-aa0b-89b00d4c3470?tenantId=41ab42a9-381b-48cf-8a85-720464922976" type="Featured Project" img={project1} github="/" />
+            <FeaturedProject title="Entrance Test App by Power Apps" summary="An application so that candidates can check their competencies when applying to the company. Done using Microsoft's low-code system - Power Apps. 
+            ( Admin: kien@gmail.com Password: kpim0 )" link="https://apps.powerapps.com/play/e/default-41ab42a9-381b-48cf-8a85-720464922976/a/24f9b052-e11b-4c73-aa0b-89b00d4c3470?tenantId=41ab42a9-381b-48cf-8a85-720464922976" type="Featured Project" img={project1} github="#" />
           </div>
           <div className='col-span-6'>
 
@@ -89,7 +92,7 @@ const Projects = () => {
 
           </div>
           <div className='col-span-12'>
-            <FeaturedProject title="Entrance Test App by Power Apps" summary="An application so that candidates can check their competencies when applying to the company. Done using Microsoft's low-code system - Power Apps." link="https://apps.powerapps.com/play/e/default-41ab42a9-381b-48cf-8a85-720464922976/a/24f9b052-e11b-4c73-aa0b-89b00d4c3470?tenantId=41ab42a9-381b-48cf-8a85-720464922976" type="Featured Project" img={project1} github="/" />
+            <FeaturedProject title="Movie API" summary="Basic project. Learning to use API from TMDB and new NextJS 13. Understanding the working flow of a fullstack project." link="https://movie-api-kyle41io.vercel.app/" type="Featured Project" img={project4} github="https://github.com/kyle41io/MovieAPI" />
 
           </div>
 
