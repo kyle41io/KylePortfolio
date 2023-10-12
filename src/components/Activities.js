1e2417123c03d10166b589806d010bfe75b62f17
kyle41io <vankien41hust@gmail.com>
2023-10-12 14:00:34 +0700

    modify Oct/2023

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,58 +1,83 @@
-import React, { useRef } from 'react'
-import { motion, useScroll } from 'framer-motion'
-import LiIcon from './LiIcon'
-import Education from './Education'
-import AnimatedText from './AnimatedText'
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
+import LiIcon from "./LiIcon";
+import Education from "./Education";
+import AnimatedText from "./AnimatedText";
 
-const Details = ({position, company, companyLink, time, address, work}) => {
+const Details = ({ position, company, companyLink, time, address, work }) => {
+  const ref = useRef(null);
 
-  const ref = useRef(null)
-
-  return ( 
-    <li ref={ref} className='my-8 md: first:mt-0 last:mb-0 mx-auto w-[60%] md:w-[80%] sm:w-[90%] flex flex-col items-center justify-between'>
+  return (
+    <li
+      ref={ref}
+      className="my-8 md: first:mt-0 last:mb-0 mx-auto w-[60%] md:w-[80%] sm:w-[90%] flex flex-col items-center justify-between"
+    >
       <LiIcon reference={ref} />
-      <motion.div initial={{y:50}} whileInView={{y:0}} transition={{duration:0.5, type:"spring"}}>
-        <h3 className='capitalize font-bold text-2xl text-dark dark:text-light'>{position}&nbsp;<a href={companyLink}
-        target="_blank" className='text-myblue dark:text-primaryDark dark:text-shadow-DarkShadow capitalize'>@{company}</a></h3>
-        <span className='capitalize font-medium text-dark/75 dark:text-light/90 '>
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl text-dark dark:text-light">
+          {position}&nbsp;
+          <a
+            href={companyLink}
+            target="_blank"
+            className="text-myblue dark:text-primaryDark dark:text-shadow-DarkShadow capitalize"
+          >
+            @{company}
+          </a>
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/90 ">
           {time} | {address}
         </span>
-        <p className='font-medium w-full text-dark dark:text-light'>{work}</p>
+        <p className="font-medium w-full text-dark dark:text-light">{work}</p>
       </motion.div>
-    </li>)
- 
-}
+    </li>
+  );
+};
 
 const Experience = () => {
   const ref = useRef(null);
-  const {scrollYProgress} = useScroll(
-    {
-      target: ref,
-      offset: ["start end", "center start"]
-    }
-  )
-  
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
   return (
     <>
-    <div id='activities' className='my-64'>
-      
-      <AnimatedText text="Experience" className='mb-16'/>
-      <div ref={ref} className='w-[75%] mx-auto relative'>
-      
-        {/* <motion.div style={{scaleY: scrollYProgress}} className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'/> */}
+      <div id="activities" className="my-64">
+        <AnimatedText text="Experience" className="mb-16" />
+        <div ref={ref} className="w-[75%] mx-auto relative">
+          <motion.div
+            style={{ scaleY: scrollYProgress }}
+            className="absolute left-9 top-0 w-[4px] h-full bg-neutral-600 origin-top"
+          />
 
-        <ul className='w-full flex flex-col items-start justify-between ml-10 md:ml-5 sm:ml-2'>
-          <Details position="Software Engineer" company="KPIM" companyLink="https://kpim.vn"  time="3/2023 - Present" address="Tay Ho, Ha Noi"
-          work="The begining period, I participated in the company's intern training program, analyzed system design and built my own idea projects based on 2 development kinds (MS low-code Power Apps and code). 
-           Then I joined to contribute to the company product. I was responsible for building RestFul API and Unit testing for the back-end of the E-learning system" />
-        </ul>        
-
+          <ul className="w-full flex flex-col items-start justify-between ml-10 md:ml-5 sm:ml-2">
+            <Details
+              position="Software Engineer"
+              company="KPIM"
+              companyLink="https://kpim.vn"
+              time="3/2023 - 9/2023"
+              address="Tay Ho, Ha Noi"
+              work="The begining period, I participated in the company's intern training program, analyzed system design and built my own idea projects based on 2 development kinds (MS low-code Power Apps and code). 
+           Then I joined to contribute to the company product. I was responsible for building RestFul API and Unit testing for the back-end of the E-learning system"
+            />
+            <Details
+              position="Front-end Developer"
+              company="MOR Software"
+              companyLink="https://morsoftware.com/"
+              time="9/2023 - Present"
+              address="Nam Tu Liem, Ha Noi"
+              work="Learing deeply about JavaScript, React and NextJS. Join the company projects with the front-end developer position..."
+            />
+          </ul>
+        </div>
       </div>
-    </div>
-    <Education />
+      <Education />
     </>
-    
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,19 +1,32 @@
-import { motion } from 'framer-motion';
-import Link from 'next/link'
-import React from 'react'
+import { motion } from "framer-motion";
+import Link from "next/link";
+import React from "react";
 
 const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
-    <div className='flex items-center justify-center'>
-      <MotionLink href="/" className='w-20 lg:w-18 sm:w-16 h-10 lg:h-9 sm:h-8 text-[25px] lg:text-[22px] sm:text-xl bg-blue-900 text-yellow-200  flex items-center justify-center rounded-md font-bold dark:shadow-sm' 
-      whileHover={{scale:1.2, backgroundColor:["#1E3A8A", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#1E3A8A"], transition:{duration: 1.5, repeat: Infinity}}}
+    <div className="flex items-center justify-center">
+      <MotionLink
+        href="/"
+        className="w-20 lg:w-18 sm:w-16 h-10 lg:h-9 sm:h-8 text-[25px] lg:text-[22px] sm:text-xl bg-blue-900 text-yellow-200 flex items-center justify-center rounded-md font-bold dark:shadow-sm"
+        whileHover={{
+          scale: 1.2,
+          backgroundColor: [
+            "#1E3A8A",
+            "rgba(131,58,180,1)",
+            "rgba(253,29,29,1)",
+            "rgba(252,176,69,1)",
+            "rgba(131,58,180,1)",
+            "#1E3A8A",
+          ],
+          transition: { duration: 1.5, repeat: Infinity },
+        }}
       >
         Kyle
       </MotionLink>
     </div>
-  )
-}
+  );
+};
 
-export default Logo
+export default Logo;
