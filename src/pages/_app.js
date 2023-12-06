f3b4af9b9b8cfa9af3d7cdd425a7997b41b9ceaa
kyle41io <vankien41hust@gmail.com>
2023-12-06 16:16:43 +0700

    add: scroll check

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,36 +1,51 @@
-import '@/styles/globals.css'
-import Head from 'next/head'
-import {Montserrat} from "next/font/google"
-import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
-import About from '@/components/About'
-import ToTop from '@/components/ToTop'
-import Experience from '@/components/Activities'
-import Projects from '@/components/Projects'
+import React from "react";
+import { Montserrat } from "next/font/google";
+
+import Head from "next/head";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import About from "@/components/About";
+import ToTop from "@/components/ToTop";
+import Experience from "@/components/Activities";
+import Projects from "@/components/Projects";
+import "@/styles/globals.css";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
-  variable: "--font-mont"
-})
+  variable: "--font-mont",
+});
 export default function App({ Component, pageProps }) {
+  const [scrollPosition, setScrollPosition] = React.useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${montserrat.variable} !scroll-smooth font-mont bg-light w-full min-h-screen dark:bg-dark overflow-x-hidden`}>
+      <main
+        className={`${montserrat.variable} !scroll-smooth font-mont bg-light w-full min-h-screen dark:bg-dark overflow-x-hidden`}
+      >
         <NavBar />
         <Component {...pageProps} />
         <About />
         <Experience />
         <Projects />
-        <ToTop />
+        {scrollPosition > 300 && <ToTop />}
         <Footer />
       </main>
     </>
-    
-    )
-
- 
+  );
 }
